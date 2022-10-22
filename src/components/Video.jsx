c9646de6553bbc3f4ab5edbcc1fb48eff0b6fcc4
Srujan <srujan@Srujans-MacBook-Air.local>
2022-10-22 10:50:23 +0530

    review

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <div className="my-20">
       <div className="flex flex-col justify-center">
-        <video src={chitti} autoPlay controls autop />
+        <video src={chitti} autoPlay controls />
         <div className="text-center my-3">
           <button className="btn btn-active btn-info ">
             <a href="#"> Register for my Webinar</a>
